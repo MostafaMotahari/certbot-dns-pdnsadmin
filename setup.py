f06a83608057d5b2399fd398714510abcef81f3b
Mostafa Motahari <mostafamotahari2004@gmail.com>
2025-04-08 15:08:12 +0330

    build: update setup.py and add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,24 +1,43 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='certbot-dns-pdnsadmin',
-    version='0.1.1',
-    description='PowerDNS Admin DNS Authenticator plugin for Certbot',
-    url='https://github.com/MostafaMotahari/certbot-dns-pdnsadmin',
-    author='Mostafa Motahari',
-    author_email='mostafamotahari2004@gmail.com',
-    license='MIT License',
-    python_requires='>=3.6',
-    packages=['certbot_dns_pdnsadmin'],
+    name="certbot-dns-pdnsadmin",
+    version="0.1.1",
+    author="Mostafa Motahari",
+    author_email="mostafamotahari2004@gmail.com",
+    description="PowerDNS Admin DNS Authenticator plugin for Certbot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MostafaMotahari/certbot-dns-pdnsadmin",
+    packages=find_packages(),
+    python_requires=">=3.6",
     install_requires=[
-        'acme>=0.31.0',
-        'certbot>=0.31.0',
-        'requests>=2.20.0',
-        'zope.interface',
+        "acme>=0.31.0",
+        "certbot>=0.31.0",
+        "requests>=2.20.0",
+        "zope.interface",
     ],
     entry_points={
-        'certbot.plugins': [
-            'dns-pdnsadmin = certbot_dns_pdnsadmin:Authenticator',
+        "certbot.plugins": [
+            "dns-pdnsadmin = certbot_dns_pdnsadmin:Authenticator",
         ],
     },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Security",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Networking",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
 )
